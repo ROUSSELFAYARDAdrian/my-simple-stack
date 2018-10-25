39843b8e9f2dbe2ab1a1af65355b801ceeaf2306
Adrian <ajy.roussel@gmail.com>
2018-10-25 08:39:23 +0200

    TP2

---
+++ src/main/java/deqo/arou/mysimplestack/App.java
@@ -0,0 +1,13 @@
+package deqo.arou.mysimplestack;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
